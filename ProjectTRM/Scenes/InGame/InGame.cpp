5dea50b70d7b909aa8c8b82018a423ee890a6ecd
https-aya <138739258+https-aya@users.noreply.github.com>
2025-06-10 15:59:46 +0900

    ガーディアンのコスト管理がおかしかったので修正

--- ProjectTRM/Scenes/InGame/InGame.cpp
+++ ProjectTRM/Scenes/InGame/InGame.cpp
@@ -167,7 +167,7 @@ eSceneType InGame::Update(const float& delta_second)
 	int Pcount_sum = 0;
 	int Ecount_sum = 0;
 
-	Pcount_sum = (int)P_Tank::GetCount() + (int)P_Melee::GetCount() + (int)P_Ranged::GetCount() ;
+	Pcount_sum = (int)P_Tank::GetCount() + (int)P_Melee::GetCount() + (int)P_Ranged::GetCount() + (int)P_Guardian::GetCount() ;
 	Ecount_sum = (int)E_Tank::GetCount() + (int)E_Melee::GetCount() + (int)E_Ranged::GetCount()	;
 
 	if (max_unit < Pcount_sum)
@@ -608,7 +608,7 @@ void InGame::UnitSelection()
 		case 3:
 			if (summon_flag[cursor] == false)
 			{
-				if (cost - Melee_Cost >= 0)
+				if (cost - Guardian_Cost >= 0)
 				{
 					// ‹ßÚ‚ð¶¬
 					GameObject* obj = object->CreateObject<P_Guardian>(Vector2D(player->GetLocation().x, player->GetLocation().y + 30.0f));
